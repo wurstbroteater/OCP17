e018a1614853a671f37b285804eeeecc2c8b4206
wurstbroteater <fleri94@web.de>
2024-03-09 11:21:17 +0100

    Update utilities

--- util/Util.java
+++ util/Util.java
@@ -1,6 +1,17 @@
 package util;
 public class Util {
-  public static void print(Object o) {
+  
+  public static void print() {
+    System.out.println();
+  }
+  
+  public static void print(final Object o) {
     System.out.println(o.toString());
+  }
+
+  public static void print(final String id, final Object o) {
+    String formatted = id.trim();
+    formatted = (formatted.endsWith(":") ? formatted : formatted + ":") + " ";
+    System.out.println(formatted + o.toString());
   }
 }
